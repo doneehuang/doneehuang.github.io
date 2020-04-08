573d2037a3c1bab3d96317a4cfa7ae3eed6b49a7
Donee Working <>
2020-04-08 14:19:03 +0800

    no message

---
+++ ig/js/main.js
@@ -0,0 +1,20 @@
+
+var $ = jQuery;
+$(function() {
+$(".instagram").instastory({
+  get: "#kkday",	
+  limit: 9,
+  imageSize: 240,	
+  template: '<div class="post"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{accessibility_caption}}"><span class="meta"><p>{{caption}}</p><span class="stats"><span class="likes"><i class="fa fa-heart"></i>{{likes}}</span><span class="comments"><i class="fa fa-comment"></i>{{comments}}</span></span></a></div>',	
+});
+	
+    $(".btn_new").click(function() {
+        $('.btn_new').hide(1000);	
+        $('.instagram').instastory({
+          get: "#kkday",			
+		  limit: 100,
+		  imageSize: 240,	
+		  template: '<div class="post"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{accessibility_caption}}"><span class="meta"><p>{{caption}}</p><span class="stats"><span class="likes"><i class="fa fa-heart"></i>{{likes}}</span><span class="comments"><i class="fa fa-comment"></i>{{comments}}</span></span></a></div>',			
+});
+    });
+});
