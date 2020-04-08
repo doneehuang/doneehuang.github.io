1df7849a84865049de5bea121e054ef57d3b0360
Donee Working <>
2020-04-08 16:06:06 +0800

    no message

--- ig/js/main.js
+++ ig/js/main.js
@@ -12,7 +12,7 @@ $(".instagram").instastory({
         $('.btn_new').hide(1000);	
         $('.instagram').instastory({
           get: "#kkday",			
-		  limit: 100,
+		  limit: 90,
 		  imageSize: 240,	
 		  template: '<div class="post"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{accessibility_caption}}"><span class="meta"><p>{{caption}}</p><span class="stats"><span class="likes"><i class="fa fa-heart"></i>{{likes}}</span><span class="comments"><i class="fa fa-comment"></i>{{comments}}</span></span></a></div>',			
 });
